d24b2e99a3c05fd685e74bc93524ee2f6989cf12
void <49158033+Narick09@users.noreply.github.com>
2021-05-27 21:56:48 +0700

    Update console.sql

--- console.sql
+++ console.sql
@@ -1,229 +1,116 @@
-CREATE TABLE Persons
+drop table if exists people;
+drop table if exists Personal_data;
+drop table if exists Telephone_book;
+drop table if exists Buyers;
+drop table if exists Owners;
+drop table if exists Agents;
+drop table if exists Object;
+drop table if exists Deal;
+drop table if exists Object_parameters;
+drop table if exists Street_list;
+drop table if exists Buildings;
+
+create table People(
+    personId serial,
+	surname	varchar(100),
+	name	varchar(100),
+	patronymic	varchar(100),
+	primary key (personId)
+);
+
+create table Personal_data(
+    personId int,
+    pasport_number varchar(11) unique not null,--need to check pattern
+    foreign key (personId) references People(personId),
+    primary key (personId)
+);
+
+create table Telephone_book(
+    personId int,
+    telephone_number varchar(15) unique not null,--need to check pattern
+    foreign key (personId) references People(personId),
+	primary key (personId)
+);
+create table Buyers
 (
-    "Pasport data" int PRIMARY KEY,
-    Surname varchar(60) not null,
-    Name varchar(60) not null,
-    Login varchar(60) UNIQUE,
-    Email varchar(60) UNIQUE,
-    Department varchar(60) check (department in ('администрация', 'бухгалтерия', 'производство', 'поддержка пользователей')),
-    city varchar(20)
+    buyerId            int,
+    reliability_factor int,
+    foreign key (buyerId) references People(personId),
+	primary key (buyerId)
 );
 
-CREATE TABLE Project
+create table Owners
 (
-    Name varchar(100) primary key,
-    Description text,
-    "Start project date" date not null,
-    "Ending project date" date
+    ownerId int,
+    foreign key (ownerId) references People(personId),
+	primary key (ownerId)
 );
 
-CREATE TABLE Task
+create table Agents
 (
-    taskID int PRIMARY KEY,
-    "project ownership" varchar,
-    Name varchar(100),
-    Priority int,
-    Description text,
-    Status varchar(12) check (status in ( 'Новая', 'Переоткрыта', 'Выполняется', 'Закрыта' )),
-    "Required time" int,
-    "spent time" int,
-    "Ending task date" date,
-    "starting task date" date,
-    Executing varchar,
-    Creator varchar,
-    FOREIGN KEY ("project ownership") REFERENCES project (Name),
-    FOREIGN KEY (executing) REFERENCES Persons (login),
-    FOREIGN KEY (Creator) REFERENCES Persons (login)
+    agentId int,
+    work_stage int,
+    foreign key (agentId) references People(personId),
+	primary key (agentId)
 );
 
---------------------------------------------------------------------------------
-INSERT INTO Persons VALUES ('1','Касаткин','Артем','ArtyomKas01','ArtyomKas01@mail.ru','администрация',null);
-INSERT INTO Persons VALUES ('2','Петрова','София','SofiaPet','SofiaPet@mail.ru','бухгалтерия',null);
-INSERT INTO Persons VALUES ('3','Дроздов','Федор','Drozd200','Drozd200@mail.ru','бухгалтерия','Москва');
-INSERT INTO Persons VALUES ('4','Иванова','Василина','Ivas9','Ivas9@mail.ru','бухгалтерия',null);
-INSERT INTO Persons VALUES ('5','Беркут','Алексей','bercal','bercal@mail.ru','производство','Владивосток');
-INSERT INTO Persons VALUES ('6','Белова','Вера','Belochka','Belochka@mail.ru','администрация','Москва');
-INSERT INTO Persons VALUES ('7','Макенрой','Алексей','makentoshAl','makentoshAl@mail.ru','производство','Новосибирск');
-
-INSERT INTO Project VALUES ('РТК',NULL,'2016-01-31',NULL);
-INSERT INTO Project VALUES ('СС.Коннект',NULL,'2015-02-23','2016-12-31');
-INSERT INTO Project VALUES ('Демо-Сибирь',NULL,'2015-05-11','2015-01-31');
-INSERT INTO Project VALUES ('МВД-Онлайн',NULL,'2015-05-22','2016-01-31');
-INSERT INTO Project VALUES ('Поддержка',NULL,'2016-06-07',NULL);
-
-insert into Task (taskID, "project ownership",Name,Priority, Status, "Ending task date", Creator) VALUES
-(1, 'РТК', 'Create DB', 1, 'Выполняется', '2021-03-11', 'ArtyomKas01'),
-(2, 'МВД-Онлайн', 'Watch DB', 55, 'Переоткрыта', '2021-03-11', 'Belochka'),
-(3, 'Демо-Сибирь', 'Do Something', 15, 'Выполняется', '2021-03-11', 'bercal'),
-(4, 'Поддержка', 'Create host-server', 12, 'Новая', '2021-03-11', 'makentoshAl');
-
-update Task set priority=100 where name='Create DB';
-update Task set Executing='Drozd200' where name='Create DB';
-update Task set Executing='makentoshAl' where name='Do Something';
-update Task set Executing='Drozd200' where name='Create host-server';
-update Task set Executing='bercal' where name='Read man(documentation)';
-update Task set Executing='SofiaPet' where name='Watch DB' and "project ownership"='СС.Коннект';
---update Task set Creator='bercal' where name='Do Something';
-insert into Task (taskID, "project ownership",Name,Priority, Status, "Ending task date", Creator) VALUES
-(6, 'СС.Коннект', 'Read man(documentation)', 51, 'Новая', '2021-03-11', 'ArtyomKas01'),
-(5, 'СС.Коннект', 'Watch DB', 0, 'Закрыта', '2021-03-11', 'Ivas9');
-
-insert into Task (taskID, "project ownership",Name,Priority, Status, "Ending task date", Creator, executing) VALUES
-(7, 'Демо-Сибирь', 'Write the article', 5, 'Новая', '2016-01-01', 'SofiaPet' ,'ArtyomKas01'),
-(8, 'Поддержка', 'Call to ArtyomKas01', 2, 'Переоткрыта', '2016-01-01', 'Ivas9', 'ArtyomKas01'),
-(9, 'Демо-Сибирь', 'Build home', 57, 'Новая', '2016-01-02', 'SofiaPet', 'ArtyomKas01'),
-(10, 'Поддержка', 'Call to ArtyomKas01', 2, 'Переоткрыта', '2016-01-01', 'Ivas9', 'ArtyomKas01');
-update task set "starting task date"='2016-01-01' where ("Ending task date" between '2016-01-01' AND '2016-01-03');
-update task set name='Call to mariaDB' where taskid=10;
-update task set name='reload system' where taskid=8;
-update task set name='Build home' where taskid=9;
-insert into Task (taskID, "project ownership",Name,Priority, Status, "Ending task date", Creator, executing) VALUES
-(11, 'СС.Коннект', 'Calculate discriminant', 5, 'Новая', '2016-01-01', 'ArtyomKas01' ,'SofiaPet'),
-(12, 'Поддержка', 'Solve problem', 2, 'Переоткрыта', '2016-01-01', 'Ivas9', 'SofiaPet');
-insert into Task (taskID, "project ownership",Name,Priority, Status, "Ending task date", Creator) VALUES
-(13, 'СС.Коннект', 'Remake product', 13, 'Новая', '2021-03-11', 'ArtyomKas01'),
-(14, 'СС.Коннект', 'Show presentation', 0, 'Закрыта', '2021-03-11', 'Ivas9');
------------------------------------------------------------------------------------
---3a
-SELECT * from Task;
-SELECT Task.* from Task;
---3b
-select Surname, Name, Department from persons;
---3c
-select login, Email from persons;
---3d
-select name, Priority from Task where Priority>50;
---3e
-select persons.Surname,persons.name, task.Name from persons,task where Persons.login=task.Executing;
---3f
-select executing from task
-union
-select Creator from task;
---3k
-select name, Executing, Creator from task where Creator!='SofiaPet' and Executing in ('Ivas9', 'bercal', 'Drozd200');
---4
-select name, Executing, "starting task date" from task where Executing='ArtyomKas01' and ("starting task date" between '2016-01-01' AND '2016-01-03');
---5
-select name,Creator from task where Executing='SofiaPet' and Creator in (select Login from persons where Department in ('администрация', 'бухгалтерия', 'производство'));
-select task.name,Creator from task, persons where Executing='SofiaPet' and Creator=login and Department in ('администрация', 'бухгалтерия', 'производство');
---6.2
-select name, Executing from task where Executing is null;
---6.3
-update task set executing='SofiaPet' where executing is null;
-select name, Executing from task where Executing='SofiaPet';
---update task set executing=null where taskID in (5,13,14);
-
---7
-drop table if EXISTS Task2;
-create table task2 as select * from Task;
-select * from task2;
---8
-select * from persons where
-                            name not like '%а'
-                        and Surname not like '%а'
-                        and login like 'b%'
-                        and login like '%r%';
----------------------------------------------------------------------------------#2----------------------------------------------------------------------------
---2.1
-select Persons.Name, Persons.Surname, avg(Priority)
-from persons, task
-where Persons.Login=Task.Executing
-group by(Persons.Name, Persons.Surname)----------------------------------зачем нужно группировать?
-order by avg(Priority) desc limit 3;
---2.2
---changing--
-select taskID, Creator,name, "starting task date" from task;
-update task set "starting task date"='2015-01-01' where taskID in (1,3,4);
---update task set executing='makentoshAl',"starting task date"='2015-01-01' where taskID=5;
-update task set "starting task date"='2015-01-03' where taskID=11;
-update task set "starting task date"='2015-12-31' where taskID=13;
-------------
-select concat(count(taskID),' - ', extract(MONTH from task."starting task date"),' - ',task.Creator) as tasks,
-       persons.surname, persons.name
-from Persons, task
-where Persons.Login=Task.Creator
-  and extract(YEAR from task."starting task date")=2015
-  and task."starting task date" is not null
-group by extract(MONTH from "starting task date"), task.Creator, persons.surname, persons.name;
---2.3
---changes--
------------
-select * from task, persons;
---2.4
---changes--
-select Creator, Executing, taskID from task;
-update task set executing='Ivas9' where taskID=14;
------------
-select distinct Creator, Executing from task  where Executing<Creator
-union
-select distinct Executing, Creator from task  where Executing>Creator
-union
-select distinct Creator, Executing from task  where Executing=Creator
-order by Creator;---------------------------------------------------как убрать комбинации, которые тут будут и что такое лексикографическая проверка
---check--
-select distinct Creator, Executing from task  where Executing<Creator;
-select distinct Executing, Creator from task  where Executing>Creator;
-select distinct Creator, Executing from task  where Executing=Creator;
----------
---2.5
-select login, length(login)
-from persons
-order by length(login) desc
-limit 1;
---2.6
-drop table if exists char_table, varchar_table;
-create table char_table(
-    cStr char(32)
+create table Object(
+    objectId int,
+    agentId	int,
+    ownerId	int,
+    primary key (objectId),
+    foreign key (agentId) references Agents(agentId),
+    foreign key (ownerId) references Owners(ownerId)
 );
 
-create table varchar_table(
-    vStr varchar(32)
+create table Deal(
+    dealId int,
+    agentId	int,
+    objectId int,
+    buyerId int,
+    deal_type varchar check (deal_type in('Usual', 'Alternative')),--it can be null
+    primary key (dealId),
+    foreign key (agentId) references Agents(agentId),
+    foreign key (objectId) references Object(objectId),
+    foreign key (buyerId) references Buyers(buyerId)
 );
-insert into char_table values ('some string');
-insert into varchar_table values ('some string');
 
-select pg_column_size(cStr) "char", pg_column_size(vStr) "varchar"
-from char_table,
-     varchar_table;
---2.7
-select persons.name, persons.Surname, max(task.Priority), task.Name as "task name"
-from Task, persons
-where task.Executing=Persons.Login
-  and Priority in
-      (select max(task.Priority)
-       from task,
-            persons
-       where task.Executing = Persons.Login
-      group by persons.name, persons.Surname)
-group by persons.name, persons.Surname, task.Name;
-------
--- select persons.name, persons.Surname, max(task.Priority), task.Name as "task name"
--- from Task, persons
--- where task.Executing=Persons.Login
--- group by persons.name, persons.Surname
--- having task.Name.max(task.Name);
+create table Object_parameters(
+    objectId int,
+    obj_type varchar check (obj_type in ('Комната', 'Квартира')),
+    price int,
+    floor_plan varchar check (obj_type in ('Секционка', 'Улучшенной планировки', 'Коммунальная, Хрущевка', 'Студия', 'Типовая', 'Элитная', 'Сталинка', 'Ленинка', 'Брежневка')),
+    living_area	int,
+    total_area int,
+    floor int,
+    amonut_of_rooms	int,
+    object_num int,
+    furniture_presence bool,
+    repair_presence bool,
+    additional_description varchar(10000),
+    primary key(objectId),
+    foreign key (objectId) references Object(objectId)
+);
 
---2.8
----changes---
-select name, "Required time" from task;
-update task set "Required time"=55 where name in ('Watch DB','Write the article', 'Remake product', 'Show presentation', 'Create DB');
-update task set "Required time"=27 where name in ('Do Something', 'Create host-server','Read man(documentation)');
-update task set "Required time"=44 where name in('Call to mariaDB','Build home', 'Create host-server');
-update task set "Required time"=13  where name in('Solve problem', 'Calculate discriminant', 'reload system');
--------------
-select Persons.Name, Persons.Surname, sum(task."Required time")
-from task, persons
-where task.Executing=Persons.Login
-  and task."Required time">(select avg("Required time") from task)
-  and task.Status in ( 'Новая', 'Переоткрыта', 'Выполняется')
-group by Persons.Name, Persons.Surname, Task."Required time";
---2.9
-with tr (taskID, executing)
-as
-(
-    select taskID from task
-    UNION ALL
-    select taskID from task
-)
-select * from task
+create table Street_list(
+    district varchar,
+    street varchar,
+    amount_selling_objects int,
+    amount_buildings int,
+    primary key (district,street)
+);
+
+create table Buildings(
+    buildingId	int,
+    objectId int,
+    district varchar,
+    street	varchar,
+    floors_amount int,
+    repair_presence	bool,
+    building_year date,
+    wall_material varchar,
+    primary key (buildingId),
+    foreign key (objectId) references Object(objectId),
+    foreign key (district, street) references Street_list(district, street)
+);
 
